c11d2fd3db83e140abd6a011dc535acbdcc007fe
Bettee0885 <b44786280@gamil.com>
2025-01-26 23:20:51 +0300

    doen

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5">Trending Movies 🍿</h2>
+        <p>Keep up with the hottest movies that are trending this week.</p>
+      </div>
+
+      <div class="my-4">
+        <a href="#" @click="getTrendingMovies('day')" class="mx-3 h4">
+          Trending today</a
+        >
+        <a href="#" @click="getTrendingMovies('week')" class="mx-3 h4"
+          >This week</a
+        >
+      </div>
+
+      <div class="row" v-if="movies.length > 0">
+        <div class="col-md-3" v-for="(movie, i) in movies" :key="i">
+          <movie-card :movie="movie" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieCard from "./components/MovieCard.vue";
+export default {
+  name: "App",
+  components: {
+    MovieCard,
+  },
+  data() {
+    return {
+      movies: [],
+      apiKey: "a0350f8d0e35f46e8c4111324a642722",
+    };
+  },
+  methods: {
+    getTrendingMovies(category) {
+      return fetch(
+        `https://api.themoviedb.org/3/trending/movie/${category}?api_key=${this.apiKey}
+        
+        `
+       
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.movies = data.results;
+        });
+    },
+  },
+  mounted() {
+    this.getTrendingMovies("day");
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: grey;
+}
+</style>
